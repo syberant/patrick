7954c6d4d56290f8585711ce55b4a5a9f8ba0470
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2021-04-23 21:56:48 +0200

    Add ask_confirmation function
    Useful for doing destructive or otherwise dangerous operations

--- r_and_d_discord_bot/__init__.py
+++ r_and_d_discord_bot/__init__.py
@@ -1,4 +1,36 @@
+import asyncio
+from discord import Embed, Colour
 from discord.ext import commands
+
+
+async def ask_confirmation(ctx, msg: str) -> bool:
+    """Asks the user for confirmation."""
+
+    embed = Embed(title="Confirmation", description=msg)
+    message = await ctx.send(embed=embed)
+    await asyncio.gather(message.add_reaction("👍"), message.add_reaction("👎"))
+
+    def check(reaction, user):
+        return user == ctx.author and message == reaction.message and reaction.emoji in ["👍", "👎"]
+
+    try:
+        reaction, user = await ctx.bot.wait_for("reaction_add", timeout=30, check=check)
+    except asyncio.TimeoutError:
+        embed.title = "Timed out"
+        embed.colour = Colour.red()
+        await asyncio.gather(message.edit(embed=embed), message.clear_reactions())
+        return False
+    else:
+        if reaction.emoji == "👎":
+            embed.title = "Denied"
+            embed.colour = Colour.red()
+            await asyncio.gather(message.edit(embed=embed), message.clear_reactions())
+            return False
+        elif reaction.emoji == "👍":
+            embed.title = "Confirmed"
+            embed.colour = Colour.green()
+            await asyncio.gather(message.edit(embed=embed), message.clear_reactions())
+            return True
 
 
 def main():
