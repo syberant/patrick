65b434688f3ebcd1281af34c7141c38ccbb2a26d
Splinter Suidman <splinter@mannenopdemaan.nl>
2021-05-13 14:11:51 +0200

    Only allow commands in a channel named #admin

--- r_and_d_discord_bot/__init__.py
+++ r_and_d_discord_bot/__init__.py
@@ -1,15 +1,29 @@
 import discord
-from discord.ext import commands
+from discord import TextChannel
+from discord.ext.commands import Bot, Context, CheckFailure
 from r_and_d_discord_bot.cogs import Development, Groups, StandardChannels
+
+
+class OnlyAdminCommands(CheckFailure):
+    pass
 
 
 def main():
     intents = discord.Intents.default()
     intents.members = True
-    bot = commands.Bot(command_prefix='>', intents=intents)
+    bot = Bot(command_prefix='>', intents=intents)
 
     bot.add_cog(Development(bot))
     bot.add_cog(Groups(bot))
     bot.add_cog(StandardChannels(bot))
+
+    @bot.check
+    async def admin_channel(ctx: Context) -> bool:
+        if isinstance(ctx.channel, TextChannel):
+            if ctx.channel.name == "admin":
+                return True
+            raise OnlyAdminCommands("Command sent in non-admin channel")
+        raise OnlyAdminCommands("Command not sent in text channel of server," +
+                                " so not in admin channel")
 
     bot.run('ODM0NzU0MjM2NjIwMzQxMjY4.YIFfdg.cAu7OaLqawMf0S3KI_b_kcFPqlY')
