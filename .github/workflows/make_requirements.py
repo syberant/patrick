5f9d8e53697930431ec2db4366ced7a6bb453137
Splinter Suidman <splinter@mannenopdemaan.nl>
2021-05-27 11:54:58 +0200

    Run all GitHub Actions checks using Nix

--- .github/workflows/make_requirements.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import configparser
-
-c = configparser.ConfigParser()
-c.read("setup.cfg")
-
-with open("requirements.txt", "w") as f:
-    f.write(c["options"]["install_requires"])
